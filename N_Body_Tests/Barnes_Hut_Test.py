862f0087641b4bf38a5007f83d858b4340b63130
Colton Quirk <coltonq@arizona.edu>
2025-01-22 16:41:00 -0700

    Changed Body to Node as that is needed for the QuadTree structure.

--- N_Body_Tests/Barnes_Hut_Test.py
+++ N_Body_Tests/Barnes_Hut_Test.py
@@ -2,10 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-class Body:
-    def __init__(self, mass, position, velocity):
-        self.m = mass
-        self.p = position
-        self.v = velocity
-    
+class node:
+    def __init__(self, x, y, px, py, m):
+        self.m = m
+        self.pos = np.array([x, y])
+        self.mom = np.array([px, py])
+        self.child = None
 
+    
+    
