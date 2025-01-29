Corrected the units of mass in ParticleProperties.

Homeworks/Homework2/.ipynb_checkpoints/ParticleProperties-checkpoint.py
@@ -1,27 +1,37 @@
+# Import libraries
 import numpy as np
 import astropy.units as u
 from ReadFile import Read
 
+# This function finds properties of a 
+# particular particle
+# Inputs: filename (name of simulation file),
+#         particle_type (number specifying type of particle),
+#         particle_num (number specifying which particle)
+# Returns: dist (Magnitude of distance in kpc)
+#          vel (Magnitude of velocity in km/s)
+#          mass (Mass of the particle in solar Mass)
 def ParticleInfo(filename, particle_type, particle_num):
-    _, _, data = Read(filename)
+    _, _, data = Read(filename)                        # Read the data from the file
+    index = np.where(data['type'] == particle_type)[0] # Find the indices of the particle type
 
-    particle_num = int(particle_num)
+    # Determine the x, y, and z position of the particle
+    x = data['x'][index][particle_num]
+    y = data['y'][index][particle_num]
+    z = data['z'][index][particle_num]
 
-    x = data['x'][particle_num]
-    y = data['y'][particle_num]
-    z = data['z'][particle_num]
-
+    # Determine the distance of the particle
     dist = np.sqrt(x**2 + y**2 + z**2)*u.kpc
+    
+    # Determine the velocity of the particle in x, y, and z
+    vx = data['vx'][index][particle_num]
+    vy = data['vy'][index][particle_num]
+    vz = data['vz'][index][particle_num]
+    
+    # Determine the magnitude of the velocity of the particle
+    vel = np.sqrt(vx**2 + vy**2 + vz**2)*u.km/u.s
 
-    vx = data['vx'][particle_num]
-    vy = data['vy'][particle_num]
-    vz = data['vz'][particle_num]
-
-    vel = np.sqrt(vx**2 + vy**2 + z**2)*u.km/u.s
-
-    mass = data['m'][particle_num]*u.solMass
+    # Determine the mass of the particle
+    mass = data['m'][index][particle_num]*1e10*u.solMass
 
     return dist, vel, mass
-
-
-

Homeworks/Homework2/ParticleProperties.py
@@ -32,6 +32,6 @@ def ParticleInfo(filename, particle_type, particle_num):
     vel = np.sqrt(vx**2 + vy**2 + vz**2)*u.km/u.s
 
     # Determine the mass of the particle
-    mass = data['m'][index][particle_num]*u.solMass
+    mass = data['m'][index][particle_num]*1e10*u.solMass
 
     return dist, vel, mass